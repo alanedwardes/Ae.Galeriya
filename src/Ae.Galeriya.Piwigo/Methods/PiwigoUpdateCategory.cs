d10324e7b153da329077ea34930b8551e5b19373
Alan Edwardes <alan@alanedwardes.com>
2021-11-20 22:17:03 +0000

    Implements pwg.categories.setInfo.

---
+++ src/Ae.Galeriya.Piwigo/Methods/PiwigoUpdateCategory.cs
@@ -0,0 +1,41 @@
+﻿using Ae.Galeriya.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ae.Galeriya.Piwigo.Methods
+{
+    internal sealed class PiwigoUpdateCategory : IPiwigoWebServiceMethod
+    {
+        private readonly GalleriaDbContext _context;
+
+        public string MethodName => "pwg.categories.setInfo";
+
+        public PiwigoUpdateCategory(GalleriaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<object> Execute(IReadOnlyDictionary<string, IConvertible> parameters, CancellationToken token)
+        {
+            var categoryId = parameters["category_id"].ToUInt32(null);
+
+            var category = await _context.Categories.SingleAsync(x => x.CategoryId == categoryId, token);
+            
+            if (parameters.TryGetValue("name", out var name))
+            {
+                category.Name = name.ToString(null);
+            }
+
+            if (parameters.TryGetValue("comment", out var comment))
+            {
+                category.Comment = comment.ToString(null);
+            }
+
+            await _context.SaveChangesAsync(token);
+            return null;
+        }
+    }
+}
