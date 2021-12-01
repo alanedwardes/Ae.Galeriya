9f96d89a6c4a767ee3bbba05022fd705f5a83720
Alan Edwardes <alan@alanedwardes.com>
2021-12-01 23:51:59 +0000

    Adds appsettings consumption to Startup

--- src/Ae.Galeriya.Web/Startup.cs
+++ src/Ae.Galeriya.Web/Startup.cs
@@ -1,11 +1,24 @@
 ﻿using Ae.Galeriya.Piwigo;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 
 namespace Ae.Galeriya.Console
 {
     public sealed class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json", false);
+
+            Configuration = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddLettuceEncrypt();
