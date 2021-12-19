a1f01e20aedc728e4701645569743d4061a26404
Alan Edwardes <alan@alanedwardes.com>
2021-12-19 16:49:25 +0000

    Adds temporary logging for PiwigoGetImages.

--- src/Ae.Galeriya.Piwigo/Methods/PiwigoGetImages.cs
+++ src/Ae.Galeriya.Piwigo/Methods/PiwigoGetImages.cs
@@ -5,20 +5,24 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Linq;
 using Ae.Galeriya.Core.Tables;
+using Microsoft.Extensions.Logging;
 
 namespace Ae.Galeriya.Piwigo.Methods
 {
     internal sealed class PiwigoGetImages : IPiwigoWebServiceMethod
     {
+        private readonly ILogger<PiwigoGetImages> _logger;
         private readonly IPiwigoPhotosPageGenerator _pageGenerator;
         private readonly ICategoryPermissionsRepository _permissionsRepository;
 
         public string MethodName => "pwg.categories.getImages";
         public bool AllowAnonymous => false;
 
-        public PiwigoGetImages(IPiwigoPhotosPageGenerator pageGenerator,
+        public PiwigoGetImages(ILogger<PiwigoGetImages> logger,
+            IPiwigoPhotosPageGenerator pageGenerator,
             ICategoryPermissionsRepository permissionsRepository)
         {
+            _logger = logger;
             _pageGenerator = pageGenerator;
             _permissionsRepository = permissionsRepository;
         }
@@ -27,6 +31,8 @@ namespace Ae.Galeriya.Piwigo.Methods
         {
             var page = parameters.GetOptional<int>("page") ?? 0;
             var perPage = parameters.GetOptional<int>("per_page") ?? 64;
+
+            _logger.LogWarning("Parameters: ", string.Join(",", parameters.Select(x => $"{x.Key}={x.Value}")));
 
             var category = await _permissionsRepository.EnsureCanAccessCategory(user, parameters.GetRequired<uint>("cat_id"), token);
 
