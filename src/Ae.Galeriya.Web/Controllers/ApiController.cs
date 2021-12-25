927d9d89e5fdac3d8822ef0e41b408ba1378678c
Alan Edwardes <alan@alanedwardes.com>
2021-12-25 18:02:40 +0000

    Adds ApiController with hashes:query

---
+++ src/Ae.Galeriya.Web/Controllers/ApiController.cs
@@ -0,0 +1,30 @@
+﻿using Ae.Galeriya.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ae.Galeriya.Web.Controllers
+{
+    [Authorize]
+    [Route("/api/v1")]
+    public sealed class ApiController : Controller
+    {
+        private readonly GaleriyaDbContext _dbContext;
+
+        public ApiController(GaleriyaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost("hashes:query")]
+        public async Task<string[]> QueryHashes([FromBody] string[] hashes, CancellationToken token)
+        {
+            return await _dbContext.Photos.Where(x => hashes.Contains(x.BlobId))
+                                          .Select(x => x.BlobId)
+                                          .ToArrayAsync(token);
+        }
+    }
+}
