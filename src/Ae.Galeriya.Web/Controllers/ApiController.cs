584dbdc0a7eeb75f0fcf18edbc8dd07834cd0bac
Alan Edwardes <alan@alanedwardes.com>
2021-12-28 20:56:04 +0000

    Adds migration step to photo upload, allowing categorisation of photos from folder paths.

--- src/Ae.Galeriya.Web/Controllers/ApiController.cs
+++ src/Ae.Galeriya.Web/Controllers/ApiController.cs
@@ -1,4 +1,5 @@
 ﻿using Ae.Galeriya.Core;
+using Ae.Galeriya.Core.Tables;
 using Ae.Galeriya.Piwigo;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -6,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -62,6 +65,64 @@ namespace Ae.Galeriya.Web.Controllers
             }
 
             await _photoCreator.CreatePhoto(context, fileBlobRepository, _configuration.PersistentBlobRepository(_serviceProvider), category, name, name, userId, createdOn, fileInfo, token);
+        }
+
+        private Category CreateCategoryHeirarchyFromPath(GaleriyaDbContext context, Category[] categories, string path, uint userId)
+        {
+            Category previousCategory = null;
+            foreach (var item in path.Split('/'))
+            {
+                var category = categories.SingleOrDefault(x => x.Name == item && x.ParentCategory == previousCategory);
+                if (category == null)
+                {
+                    category = new Category
+                    {
+                        Name = item,
+                        ParentCategory = previousCategory,
+                        CreatedOn = DateTime.UtcNow,
+                        CreatedById = userId
+                    };
+                    context.Categories.Add(category);
+                }
+                previousCategory = category;
+            }
+
+            return previousCategory;
+        }
+
+        [HttpPost("photos:paths")]
+        public async Task CategorisePhotos(IReadOnlyDictionary<string, string> hashPaths)
+        {
+            var userId = HttpContext.User.Identity.GetUserId();
+
+            var groups = hashPaths.GroupBy(x => Path.GetDirectoryName(x.Key))
+                .ToDictionary(x => x.Key, x => x.Select(x => x.Value).ToArray());
+
+            var allCategories = groups.Keys.SelectMany(x => x.Split('/')).ToHashSet();
+            var allHashes = groups.Values.SelectMany(x => x).ToHashSet();
+
+            using var context = _serviceProvider.GetRequiredService<GaleriyaDbContext>();
+
+            var categories = await _categoryPermissions.GetAccessibleCategories(context, userId)
+                .Where(x => allCategories.Contains(x.Name))
+                .ToArrayAsync(HttpContext.RequestAborted);
+
+            var photos = await _categoryPermissions.GetAccessiblePhotos(context, userId)
+                .Where(x => allHashes.Contains(x.BlobId))
+                .ToArrayAsync(HttpContext.RequestAborted);
+
+            foreach (var group in groups)
+            {
+                var category = CreateCategoryHeirarchyFromPath(context, categories, group.Key, userId);
+
+                foreach (var photo in photos.Where(x => group.Value.Contains(x.BlobId)))
+                {
+                    if (!category.Photos.Contains(photo))
+                    {
+                        category.Photos.Add(photo);
+                    }
+                }
+            }
         }
     }
 }
